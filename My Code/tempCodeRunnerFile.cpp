8faa78775699ea8bf9a05a3bc33572ced2edac66
ayush05singh <ayushkumar2205@gmail.com>
2022-09-15 13:30:44 +0530

    New Update

--- My Code/tempCodeRunnerFile.cpp
+++ My Code/tempCodeRunnerFile.cpp
@@ -2,21 +2,8 @@
 using namespace std;
 int main()
 {
-    int day;
-    cout<<"Enter Day No."<<endl;
-    cin>>day;
-
-    switch(day)
-    {
-        case 1: cout<<"Monday"
-        case 2: cout<<"Tuesday"
-        case 3: cout<<"Wednesday"
-        case 4: cout<<"Thursday"
-        case 5: cout<<"Friday"
-        case 6: cout<<"Saturday"
-        case 7: cout<<"Sunday"
-        default: cout<<"Error"
-        
-    }
-    return 0;
+    int year
+    cout<<"Enter year =\t";
+    cin>>year;
+    if
 }
