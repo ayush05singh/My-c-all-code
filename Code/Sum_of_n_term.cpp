ca523e6d0bbd66f10c60e50d73350d896422fdfb
ayush22singh <ayushkumar2205@gmail.com>
2022-10-03 22:38:14 +0100

    New

--- Code/Sum_of_n_term.cpp
+++ Code/Sum_of_n_term.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 int main()
 {
-	int n,s;
-	
-	cout<<"Enter no. of term - 'n'";
-	cin>>n;
-	
-	s=(n*(n+1))/2;
-	
-	cout<<"Sum of n-term"<<s;
-    return 0; 
-    
+	int n, s;
+
+	cout << "Enter no. of term - 'n'";
+	cin >> n;
+
+	s = (n * (n + 1)) / 2;
+
+	cout << "Sum of n-term" << s;
+	return 0;
 }
