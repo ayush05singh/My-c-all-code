2ca4894c2430dd4a896e18f7c927282bc7a11f25
ayush22singh <ayushkumar2205@gmail.com>
2022-10-15 10:20:53 +0530

    15-10-22

--- Code/array.cpp
+++ Code/array.cpp
@@ -3,6 +3,11 @@ using namespace std;
 int main()
 {
     int a[5] = {25, 12, 5, 63, 8};
+    int i = 0;
 
-    cout << "a1";
+    for (i; i < 850; i++)
+    {
+        cout << endl
+             << a[i] << endl;
+    }
 }
